fixed bugs in loading previously trained models - fixed bug with normalization - created out of sample environment datasets - created initial evaluation code for out-of-sample model accuracy

dmbrl/config/create_model_network.py
@@ -24,6 +24,6 @@ def build_model(model,model_in,model_out,network_shape,activations,weight_decays
             model.add(FC(hidden_units, activation=activation, weight_decay=weight_decay))
 
     model.add(FC(model_out, weight_decay=weight_decays[-1]))
-    print("\n\n\n\n this is the type of the learning rate = {} \n\n\n\n".format(type(learning_rate)))
-    model.finalize(tf.train.AdamOptimizer, {"learning_rate": learning_rate})
+    # print("\n\n\n\n this is the type of the learning rate = {} \n\n\n\n".format(type(learning_rate)))
+    # model.finalize(tf.train.AdamOptimizer, {"learning_rate": learning_rate})
     return model

dmbrl/config/gym_ant.py
@@ -124,12 +124,13 @@ class AntConfigModule:
         #     model.add(FC(200, activation="swish", weight_decay=0.000075))
         #     model.add(FC(200, activation="swish", weight_decay=0.000075))
         #     model.add(FC(self.MODEL_OUT, weight_decay=0.0001))
-        # model.finalize(tf.train.AdamOptimizer, {"learning_rate": 0.001})
             network_shape = get_required_argument(model_init_cfg,"network_shape","network shape missing!")
             activation = get_required_argument(model_init_cfg,"activation","acivations missing!")
             weight_decays = get_required_argument(model_init_cfg,"weight_decays","weight decays missing!")
             learning_rate = get_required_argument(model_init_cfg,"lr","learning rate missing!")
             model = build_model(model,self.MODEL_IN,self.MODEL_OUT,network_shape,activation,weight_decays,learning_rate)
+        model.finalize(tf.train.AdamOptimizer, {"learning_rate": get_required_argument(model_init_cfg,"lr","learning rate missing!")})
+
         return model
 
     def gp_constructor(self, model_init_cfg):

dmbrl/config/gym_hopper.py
@@ -122,7 +122,7 @@ class HopperConfigModule:
         #     model.add(FC(200, activation="swish", weight_decay=0.000075))
         #     model.add(FC(200, activation="swish", weight_decay=0.000075))
         #     model.add(FC(self.MODEL_OUT, weight_decay=0.0001))
-        # model.finalize(tf.train.AdamOptimizer, {"learning_rate": 0.001})
+        model.finalize(tf.train.AdamOptimizer, {"learning_rate": get_required_argument(model_init_cfg,"lr","learning rate missing!")})
         return model
 
     def gp_constructor(self, model_init_cfg):

dmbrl/config/halfcheetah.py
@@ -98,6 +98,7 @@ class HalfCheetahConfigModule:
             misc=misc
         ))
         if not model_init_cfg.get("load_model", False):
+            print('not building model from scratch')
             network_shape = get_required_argument(model_init_cfg,"network_shape","network shape missing!")
             activation = get_required_argument(model_init_cfg,"activation","acivations missing!")
             weight_decays = get_required_argument(model_init_cfg,"weight_decays","weight decays missing!")
@@ -108,7 +109,7 @@ class HalfCheetahConfigModule:
         #     model.add(FC(200, activation="swish", weight_decay=0.000075))
         #     model.add(FC(200, activation="swish", weight_decay=0.000075))
         #     model.add(FC(self.MODEL_OUT, weight_decay=0.0001))
-        # model.finalize(tf.train.AdamOptimizer, {"learning_rate": 0.001})
+        model.finalize(tf.train.AdamOptimizer, {"learning_rate": get_required_argument(model_init_cfg,"lr","learning rate missing!")})
             
         return model
 

dmbrl/modeling/models/BNN.py
@@ -173,8 +173,8 @@ class BNN:
         with tf.variable_scope(self.name):
             self.optimizer = optimizer(**optimizer_args)
             self.sy_train_in = tf.placeholder(dtype=tf.float32,
-                                              shape=[self.num_nets, None, self.layers[0].get_input_dim()],
-                                              name="training_inputs")
+                                            shape=[self.num_nets, None, self.layers[0].get_input_dim()],
+                                            name="training_inputs")
             self.sy_train_targ = tf.placeholder(dtype=tf.float32,
                                                 shape=[self.num_nets, None, self.layers[-1].get_output_dim() // 2],
                                                 name="training_targets")